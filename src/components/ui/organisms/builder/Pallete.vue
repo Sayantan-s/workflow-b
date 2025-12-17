fef6713162c18afb5e440d0444f36c873c0bd865
Sayantan <Sayantan@HyreSnaps-MacBook-Pro.local>
2025-12-18 00:51:39 +0530

    feat: added initial code

---
+++ src/components/ui/organisms/builder/Pallete.vue
@@ -0,0 +1,3 @@
+<script setup lang="ts">
+import { Panel, useVueFlow } from "@vue-flow/core";
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import Components from "unplugin-vue-components/vite";
+import RekaResolver from "reka-ui/resolver";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      dts: true,
+      resolvers: [RekaResolver()],
+    }),
+  ],
   server: {
     open: true,
   },
